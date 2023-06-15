2c086c859d6063325ce1dd1a1b11db60e333291b
jiae <yja2397@gmail.com>
2023-06-15 20:49:15 +0900

    delete files

--- app.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-const mysql = require("mysql2");
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-console.log("Connected to PlanetScale!");
-connection.end();
